b235b3401bac3a647b24cf6b7688458ac15f12ff
wingkwong <wingkwong.code@gmail.com>
2018-02-01 11:02:36 +0800

    revamped chameleon structure

--- chameleon.js
+++ chameleon.js
@@ -2,7 +2,8 @@
  * chameleon.js - Video with images synced
  * @author Wing Kam Wong - wingkwong.code@gmail.com
  */
-;(function(factory) {
+;
+(function(factory) {
     'use strict';
     if (typeof define === 'function' && define.amd) {
         define(['jquery'], factory);
@@ -12,157 +13,197 @@
         factory(jQuery);
     }
 }(function($) {
-	  $.fn.chameleon = function(options) {
-	  	console.log("chameleon started");
-		return this.each(function() {
-			var o = $.extend({}, $.fn.chameleon.defaults, options),
-			$this = $(this),
-			$videoWrap = '<div class="video-wrap"><div id="jwplayer"></div></div>',
-			$slideWrap = '<div class="slide-wrap"><img/></div>',
-			$previewWrap = '<div class="preview-wrap"></div>',
-			$carouselWrap = '<div class="carousel-wrap"></div>',
-			$carouselItem = '<div class="carousel-item"></div>',
-			$previewImage = '<div class="iWrap"><div class="preview-image"><img/></div><div class="slide-number"></div></div>',
-			$carouselControl = '<a class="prev sync carousel-control">&lt;</a><a class="next sync carousel-control">&gt;</a>'
+    var chameleon = 'chameleon';
 
-			_clickIndicatorHandler = function(e){
-				console.log("_clickIndicatorHandler");
-				e.preventDefault();
-				if($(this).hasClass("prev")){
-					if($this.find('.carousel-item.active').is(':first-child')){
-						 $this.find('.carousel-item.active').removeClass("active").parent()
-							 .find('.carousel-item:last-child').addClass("active");	
-					}else{
-						 $this.find('.carousel-item.active').removeClass("active")
-							 .prev().addClass("active");	
-					}
-				}else if ($(this).hasClass("next")){
-					if($this.find('.carousel-item.active').is(':last-child')){
-						 $this.find('.carousel-item.active').removeClass("active").parent()
-							 .find('.carousel-item:first-child').addClass("active");	
-					}else{
-						 $this.find('.carousel-item.active').removeClass("active")
-							 .next().addClass("active");	
-					}
-				} else{
-					return;
-				}
+    function Chameleon(element, o) {
+        var el = element;
+        var $el = $(element);
 
-			},
-			_clickPreviewImgHandler = function(){
-				console.log("_clickPreviewImgHandler");
+        o = $.extend({}, $.fn[chameleon].defaults, o);
 
-			},
-			_updateImgOrder = function(index){
-				console.log("_updateImgOrder");
+        var $chameleon = $('.chameleon'),
+            $this = $(this),
+            $videoWrap = '<div class="video-wrap"><div id="jwplayer"></div></div>',
+            $slideWrap = '<div class="slide-wrap"><img/></div>',
+            $previewWrap = '<div class="preview-wrap"></div>',
+            $carouselWrap = '<div class="carousel-wrap"></div>',
+            $carouselItem = '<div class="carousel-item"></div>',
+            $previewImage = '<div class="iWrap"><div class="preview-image"><img/></div><div class="slide-number"></div></div>',
+            $carouselControl = '<a class="prev sync carousel-control">&lt;</a><a class="next sync carousel-control">&gt;</a>',
+            jwplayerInstance = {};
 
-				 var index = parseInt(index) + 1;
-		         var target = $('.cloneditem-2 .preview-image[data-index="' + index + '"]').parent().parent();
-		         $('.carousel-item.active').removeClass("active");
-		         target.addClass("active");
-			},
-			_parseTime = function(time){
-				// parse time of type string: '00:00:15'
-		         var hms = time.split(':');
-		         return (parseInt(hms[0] * 60 * 60) + parseInt(hms[1] * 60) + parseInt(hms[2]));
-			},
 
-			methods = {
-				 method1: function(){
-				 	
-				 },
-				 method2: function(){
-				 	
-				 }
-			};
+        function _clickIndicatorHandler(e) {
+            console.log("_clickIndicatorHandler");
+            e.preventDefault();
+            if ($(this).hasClass("prev")) {
+                if ($chameleon.find('.carousel-item.active').is(':first-child')) {
+                    $chameleon.find('.carousel-item.active').removeClass("active").parent()
+                        .find('.carousel-item:last-child').addClass("active");
+                } else {
+                    $chameleon.find('.carousel-item.active').removeClass("active")
+                        .prev().addClass("active");
+                }
+            } else if ($(this).hasClass("next")) {
+                if ($chameleon.find('.carousel-item.active').is(':last-child')) {
+                    $chameleon.find('.carousel-item.active').removeClass("active").parent()
+                        .find('.carousel-item:first-child').addClass("active");
+                } else {
+                    $chameleon.find('.carousel-item.active').removeClass("active")
+                        .next().addClass("active");
+                }
+            } else {
+                return;
+            }
 
-			//expose public methods
-			if (typeof options === 'string' && $.isFunction(methods[options])) {
-	            return methods[ options ].apply( this, Array.prototype.slice.call( arguments, 1 ));
-	        }else if(typeof options === 'object' || ! options){
-	        	// do nothing
-	        }else {
-	            $.error( 'Method ' +  options + ' does not exist on chameleon.js' );
-	        }    
+        }
 
-			//-----------------CHAMLEON--------------------
+        function _clickPreviewImgHandler() {
+            console.log("_clickPreviewImgHandler");
+        }
 
-			$this.append($videoWrap);
+        function _updateImgOrder(index) {
+            console.log("_updateImgOrder");
 
-			var slides;
+            var index = parseInt(index) + 1;
+            var target = $('.cloneditem-2 .preview-image[data-index="' + index + '"]').parent().parent();
+            $('.carousel-item.active').removeClass("active");
+            target.addClass("active");
+        }
 
-			if(typeof o.slidePool === "object"){
-				if(typeof o.slidePool.slides != "undefined" && o.slidePool.slides.length > 0){
-					slides = o.slidePool.slides
-				}else{
-					return;
-				}
-			}
+        function _parseTime(time) {
+            // parse time of type string: '00:00:15'
+            var hms = time.split(':');
+            return (parseInt(hms[0] * 60 * 60) + parseInt(hms[1] * 60) + parseInt(hms[2]));
+        }
 
-			if(typeof o.slidePool === "string"){
-				var regex = /(?:\.([^.]+))?$/;
-				if(regex.exec(o.slidePool)[1] == "json"){
-					$.getJSON(o.slidePool, function(data){
-						console.log(data);
-						//TODO: test this
-						slides = data;
-					});
-				}else{
-					return;
-				}
-			}
+        // public ------------------------
 
-			$this.append($slideWrap).append($previewWrap);
+        function setJWplayerInstance(jwPlayerInst) {
+            
+            jwPlayerInst.onReady(function() {
+         jwPlayerInst.play();
+     });
+        }
 
-			// Slide Wrap
-			$this.find('.slide-wrap img').attr('src', slides[0].img);
+        function hook(hookName) {
+            if (o[hookName] !== undefined) {
+                o[hookName].call(el);
+            }
+        }
 
-			// Preview Carousel
-			$this.find('.preview-wrap').append($carouselWrap).append($carouselControl);
 
-			for(var i=1; i<=slides.length; i++){
-				var $cItem = $($carouselItem).append($previewImage);
-				$cItem.find('.preview-image').attr('data-index', i);
-				$cItem.find('.preview-image img').attr('src', slides[i-1].img);
-				$cItem.find('.slide-number').html((i) + '/' + slides.length);
-				$this.find('.carousel-wrap').append($cItem);
-			}
 
-			$this.find('.carousel-item:first').addClass("active");
+        //-----------------CHAMLEON--------------------//
 
-			if (slides.length > o.carouselSlide) {
-				$('.carousel-item').each(function() {
-                 var itemToClone = $(this);
-                 for (var i = 1; i < o.carouselSlide; i++) {
-                     itemToClone = itemToClone.next();
-                     if (!itemToClone.length) {
-                         itemToClone = $(this).siblings(':first');
-                     }
-                     itemToClone.children(':first-child').clone()
-                         .addClass("cloneditem-" + (i))
-                         .appendTo($(this));
-                 	}
-            	 });
-				_updateImgOrder(0);
-        	}else{
-        		$this.find('.carousel-item').addClass("active");
-        	}
+        $chameleon.append($videoWrap);
 
-			$this.find('.carousel-control').click(_clickIndicatorHandler);
-			$this.find('.preview-image').click(_clickPreviewImgHandler);
+        var slides;
 
-			
+        if (typeof o.slidePool === "object") {
+            if (typeof o.slidePool.slides != "undefined" && o.slidePool.slides.length > 0) {
+                slides = o.slidePool.slides
+            } else {
+                return;
+            }
+        }
 
-			//-----------------CHAMLEON--------------------
-		});
-	  };
-	  
-	  // plugin defaults
-	  $.fn.chameleon.defaults = {
-		slidePool: {}, 					// slides JSON object
-		carouselSlide: 5,				// number of slides showing in carousel
-		downloadVideo: false,			// download video button
-		downloadTranscript: false		// download transcript button
+        if (typeof o.slidePool === "string") {
+            var regex = /(?:\.([^.]+))?$/;
+            if (regex.exec(o.slidePool)[1] == "json") {
+                $.getJSON(o.slidePool, function(data) {
+                    console.log(data);
+                    //TODO: test this
+                    slides = data;
+                });
+            } else {
+                return;
+            }
+        }
 
-	  };
+
+        $chameleon.append($slideWrap).append($previewWrap);
+
+        // Slide Wrap
+        $chameleon.find('.slide-wrap img').attr('src', slides[0].img);
+
+        // Preview Carousel
+        $chameleon.find('.preview-wrap').append($carouselWrap).append($carouselControl);
+
+        for (var i = 1; i <= slides.length; i++) {
+            var $cItem = $($carouselItem).append($previewImage);
+            $cItem.find('.preview-image').attr('data-index', i);
+            $cItem.find('.preview-image img').attr('src', slides[i - 1].img);
+            $cItem.find('.slide-number').html((i) + '/' + slides.length);
+            $chameleon.find('.carousel-wrap').append($cItem);
+        }
+
+        $chameleon.find('.carousel-item:first').addClass("active");
+
+        if (slides.length > o.carouselSlide) {
+            $('.carousel-item').each(function() {
+                var itemToClone = $(this);
+                for (var i = 1; i < o.carouselSlide; i++) {
+                    itemToClone = itemToClone.next();
+                    if (!itemToClone.length) {
+                        itemToClone = $(this).siblings(':first');
+                    }
+                    itemToClone.children(':first-child').clone()
+                        .addClass("cloneditem-" + (i))
+                        .appendTo($(this));
+                }
+            });
+            _updateImgOrder(0);
+        } else {
+            $chameleon.find('.carousel-item').addClass("active");
+        }
+
+        $chameleon.find('.carousel-control').click(_clickIndicatorHandler);
+        $chameleon.find('.preview-image').click(_clickPreviewImgHandler);
+
+
+        console.log($this.jwplayerInstance);
+
+
+
+
+        return {
+            setJWplayerInstance: setJWplayerInstance
+        };
+
+        //-----------------CHAMLEON--------------------//
+    }
+
+    $.fn[chameleon] = function(o) {
+        if (typeof arguments[0] === 'string') {
+            var methodName = arguments[0];
+            var args = Array.prototype.slice.call(arguments, 1);
+            var returnVal;
+            this.each(function() {
+                if ($.data(this, 'plugin_' + chameleon) && typeof $.data(this, 'plugin_' + chameleon)[methodName] === 'function') {
+                    returnVal = $.data(this, 'plugin_' + chameleon)[methodName].apply(this, args);
+                } else {
+                    throw new Error('Method ' + methodName + ' does not exist on jQuery.' + chameleon);
+                }
+            });
+            if (returnVal !== undefined) {
+                return returnVal;
+            } else {
+                return this;
+            }
+        } else if (typeof o === "object" || !o) {
+            return this.each(function() {
+                if (!$.data(this, 'plugin_' + chameleon)) {
+                    $.data(this, 'plugin_' + chameleon, new Chameleon(this, o));
+                }
+            });
+        }
+    };
+
+    $.fn[chameleon].defaults = {
+        slidePool: {}, // slides JSON object
+        carouselSlide: 5, // number of slides showing in carousel
+        downloadVideo: false, // download video button
+        downloadTranscript: false, // download transcript button
+    };
 }));
